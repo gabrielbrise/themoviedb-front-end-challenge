61f4f045c540c79d1ea85d51ac57c2e286b8ee00
Brise <gabriel_ns@live.com>
2022-02-14 02:55:54 -0300

    add: cursor pointer to buttons with url

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 import { COLOR_PRIMARY } from '../../constants/css'
 
 const Button = ({ color = "white", backgroundColor = COLOR_PRIMARY, children, to }) => {
-    const ButtonComponent = () => <Container className="w-100 p-1" style={{ backgroundColor, color }}>{children}</Container>
-    return to ? <Link to={to}><ButtonComponent /></Link> : <ButtonComponent />
+  const ButtonComponent = () => <Container className="w-100 p-1" style={{ backgroundColor, color, cursor: to ? 'pointer' : 'default' }}>{children}</Container>
+  return to ? <Link to={to}><ButtonComponent /></Link> : <ButtonComponent />
 }
 
 export default Button
